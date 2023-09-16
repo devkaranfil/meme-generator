8254f0fbdc07cf925805d05847e636560c02fad2
karanfilberk <berk.karanfiloglu@gmail.com>
2023-09-16 13:56:59 +0300

    initiate

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export function Header() {
+  return (
+    <header>
+      <img
+        className="logo--img"
+        src="https://trollface.dk/trollfaceONE.png"
+        alt="logo"
+      />
+      <h2 className="logo--title">Meme Generator</h2>
+      <h4 className="course--title">React Course - Project 3</h4>
+    </header>
+  );
+}
---
+++ src/components/Meme.jsx
@@ -0,0 +1,23 @@
+export function Meme() {
+  return (
+    <main>
+      <form className="form" action="">
+        <input
+          type="text"
+          className="input"
+          placeholder="Top Text"
+          name=""
+          id=""
+        />
+        <input
+          type="text"
+          className="input"
+          placeholder="Bottom Text"
+          name=""
+          id=""
+        />
+        <button className="button">Get a new meme image 🖼</button>
+      </form>
+    </main>
+  );
+}
